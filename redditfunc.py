4e6d957dbd1d95b7540ba98ec17d224eb4f61145
Jason Tatem <jattdb@gmail.com>
2016-03-25 23:13:32 -0400

    First commit

---
+++ redditfunc.py
@@ -0,0 +1,40 @@
+import requests
+import time
+import re
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+import HTMLParser
+import markov
+
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
+
+def get_frontpage_links(subreddit):
+    url = 'https://www.reddit.com/r/{0}'.format(subreddit)
+    r = requests.get(url, allow_redirects=False, headers=headers)
+    thread_links = list(set([link.get('href') for link in BeautifulSoup(r.text, parseOnlyThese=SoupStrainer('a')) if link.has_key('href') and 'comments' in link.get('href')]))
+    return thread_links
+
+def get_thread_comments(thread_url):
+    r = requests.get(thread_url, allow_redirects=False, headers=headers)
+    s = BeautifulSoup(r.text, convertEntities=BeautifulSoup.HTML_ENTITIES)
+    divs = s.findAll('div', {'class': 'md'})
+    comments = [re.sub('<[^<]+?>', '', str(d).split('<p>')[1].split('</p>')[0].replace('\n', ' ')) for d in divs if '<p>' in str(d)][1:]
+    return comments 
+
+def suck_subreddit(subreddit):
+    threads = get_frontpage_links(subreddit)
+    print('Found {0} threads for /r/{1}'.format(len(threads), subreddit))
+    comments = []
+    for thread in threads:
+        if not thread.startswith('http'):
+            url = 'https://www.reddit.com{0}'.format(thread)
+        else:
+            url = thread
+	print('Fetching thread URL {0}'.format(url))
+	comments += get_thread_comments(url)
+        time.sleep(1)
+    return comments
+
+def markov_subreddit(subreddit):
+    comments = suck_subreddit(subreddit)
+    return markov.Markov(' '.join(comments))
+        
